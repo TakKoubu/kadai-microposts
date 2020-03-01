c6be4d886687221195761822a1906416208b30db
TakKoubu <kokubu.tk@gmail.com>
2020-03-01 09:57:07 +0000

    completed_without_favorite_page

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,9 +3,6 @@ class ToppagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
-      @user = User.find(params[:id])
-      @micropost = @user.microposts
-      @likes = @user.likes.page(params[:page])
     end
   end
 end

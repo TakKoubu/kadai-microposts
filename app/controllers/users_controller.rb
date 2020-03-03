90918b4ef7f1ba3ab324860db1c6ed1962492f2d
TakKoubu <kokubu.tk@gmail.com>
2020-03-03 10:52:51 +0000

    users_showから各ユーザーのお気に入り一覧の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @users = User.order(id: :desc).page(params[:page]).per(25)
     @micropost = Micropost.find_by(id: params[:id])
     @microposts = @user.microposts.order(id: :desc).page(params[:page])
     counts(@user)
